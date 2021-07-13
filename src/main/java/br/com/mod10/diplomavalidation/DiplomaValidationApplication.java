ae876494e66a537b7a619a26236bdd2f62340ca1
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-13 11:21:13 -0300

    Inicialização do projeto spring

---
+++ src/main/java/br/com/mod10/diplomavalidation/DiplomaValidationApplication.java
@@ -0,0 +1,13 @@
+package br.com.mod10.diplomavalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaValidationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiplomaValidationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/mod10/diplomavalidation/DiplomaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mod10.diplomavalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaValidationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
