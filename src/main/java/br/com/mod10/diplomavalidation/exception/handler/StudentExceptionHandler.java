0abe2af11db60e2b686a1a4559a438bd59956545
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-13 12:21:19 -0300

    Criação do handler para tratar a exceção lançada pelo Java Bean ValidatioN

---
+++ src/main/java/br/com/mod10/diplomavalidation/exception/handler/StudentExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.com.mod10.diplomavalidation.exception.handler;
+
+import br.com.mod10.diplomavalidation.dto.ExceptionFieldDTO;
+import br.com.mod10.diplomavalidation.utils.exception.FieldErrors;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+public class StudentExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> argumentNotValidHandler(MethodArgumentNotValidException e) {
+    BindingResult result = e.getBindingResult();
+
+    List<FieldError> fieldErrors = result.getFieldErrors();
+    ExceptionFieldDTO exceptions = FieldErrors.processFieldErrors(fieldErrors);
+
+    return ResponseEntity.badRequest().body(exceptions);
+  }
+}
