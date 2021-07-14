f957f019bb8f5841d3742b97edd48f9ba37454e1
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-14 09:20:47 -0300

    Refatorando o util para calcular média

--- src/main/java/br/com/mod10/diplomavalidation/utils/CalculateAverage.java
+++ src/main/java/br/com/mod10/diplomavalidation/utils/CalculateAverage.java
@@ -1,15 +1,16 @@
 package br.com.mod10.diplomavalidation.utils;
 
-import br.com.mod10.diplomavalidation.form.SubjectForm;
+import br.com.mod10.diplomavalidation.dto.SubjectDTO;
+import br.com.mod10.diplomavalidation.entity.Subject;
 
 import java.util.List;
 
 public class CalculateAverage {
 
-  public static double calcAverage(List<SubjectForm> subjects) {
+  public static double calcAverage(List<SubjectDTO> subjects) {
     double average = 0;
 
-    for (SubjectForm subject : subjects) {
+    for (SubjectDTO subject : subjects) {
       average += subject.getNote();
     }
 
