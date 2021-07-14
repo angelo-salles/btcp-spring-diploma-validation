db887b91b88245f78773efda47e56ec8bd70638b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-14 09:12:04 -0300

    Refatorando controller e adicionando novos endpoints

--- src/main/java/br/com/mod10/diplomavalidation/controller/StudentController.java
+++ src/main/java/br/com/mod10/diplomavalidation/controller/StudentController.java
@@ -1,16 +1,16 @@
 package br.com.mod10.diplomavalidation.controller;
 
+import br.com.mod10.diplomavalidation.dto.StudentDTO;
 import br.com.mod10.diplomavalidation.form.StudentForm;
 import br.com.mod10.diplomavalidation.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/aula1")
@@ -29,5 +29,22 @@ public class StudentController {
   @PostMapping("/analyzeNotes")
   public ResponseEntity<?> analyzeNotes(@RequestBody @Valid StudentForm studentForm) {
     return ResponseEntity.status(HttpStatus.CREATED).body(studentService.degree(studentForm));
+  }
+
+  @PostMapping
+  public ResponseEntity<?> saveStudent(@RequestBody @Valid StudentForm studentForm, UriComponentsBuilder uriComponentsBuilder) {
+    StudentDTO student = this.studentService.save(studentForm);
+    URI uri = uriComponentsBuilder.path("/api/aula1/student/{id}").buildAndExpand(student.getId()).toUri();
+    return ResponseEntity.created(uri).body(student);
+  }
+
+  @GetMapping("/students")
+  public ResponseEntity<?> getAllStudents() {
+    return ResponseEntity.ok().body(this.studentService.findAll());
+  }
+
+  @GetMapping("/student/{id}")
+  public ResponseEntity<?> getStudentById(@PathVariable long id) {
+    return ResponseEntity.ok(this.studentService.findById(id));
   }
 }
