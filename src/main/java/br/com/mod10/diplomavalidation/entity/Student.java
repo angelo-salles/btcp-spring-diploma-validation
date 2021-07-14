8153ad2726580ea1ba0ac22aec158aa7c81444a5
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-14 09:09:37 -0300

    Refatorando entidades para tabelas

--- src/main/java/br/com/mod10/diplomavalidation/entity/Student.java
+++ src/main/java/br/com/mod10/diplomavalidation/entity/Student.java
@@ -1,10 +1,21 @@
 package br.com.mod10.diplomavalidation.entity;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "student")
 public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String name;
+  @OneToMany(
+          cascade = CascadeType.ALL,
+          orphanRemoval = true
+  )
+  @JoinColumn(name = "student_id")
   private List<Subject> subjects = new ArrayList<>();
 
   public Student() {
@@ -15,11 +26,36 @@ public class Student {
     this.subjects = subjects;
   }
 
+  public Long getId() {
+    return id;
+  }
+
   public String getName() {
     return name;
   }
 
   public List<Subject> getSubjects() {
     return subjects;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setSubjects(List<Subject> subjects) {
+    this.subjects = subjects;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", subjects=" + subjects +
+            '}';
   }
 }
--- src/main/java/br/com/mod10/diplomavalidation/entity/Subject.java
+++ src/main/java/br/com/mod10/diplomavalidation/entity/Subject.java
@@ -1,6 +1,13 @@
 package br.com.mod10.diplomavalidation.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subject")
 public class Subject {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String subject;
   private Integer note;
 
@@ -12,11 +19,27 @@ public class Subject {
     this.note = note;
   }
 
+  public Long getId() {
+    return id;
+  }
+
   public String getSubject() {
     return subject;
   }
 
   public Integer getNote() {
     return note;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public void setNote(Integer note) {
+    this.note = note;
   }
 }
